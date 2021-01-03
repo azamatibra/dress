02e35bde8fd2a4a73c6b0d33cb78674d7267de4b
Musa <black.nobody.anb@gmail.com>
2021-01-03 19:33:44 +0600

    version 0.1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,33 @@
-let burger = document.querySelector('.hamburger')
+let burger = document.querySelector(".hamburger");
 
-burger.addEventListener('click', (e) => {
+burger.addEventListener("click", (e) => {
   e.preventDefault();
-  burger.classList.toggle('is-active')
-})
+  burger.classList.toggle("is-active");
+});
+
+const swiper = new Swiper(".swiper-container", {
+  navigation: {
+    nextEl: ".catalog__button-next",
+    prevEl: ".catalog__button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+  loop: true,
+  watchOverflow: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1170: {
+      slidesPerView: 3,
+    },
+  },
+});
